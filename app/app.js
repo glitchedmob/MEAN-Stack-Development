24892b26641db4b22feb247e7c1552c24514fc4b
Levi Zitting <levizitting5663@gmail.com>
2016-11-29 20:07:22 -0600

    Commit to switch computers
    This is just a commit to update the code in the repo so I can switch PCs

--- app/app.js
+++ app/app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.set("port", 3000);	
+app.set("port", 3000);
+
+app.get("/", function(req, res) {
+	console.log("GET the homepage");
+	res.send("Express yourself!<a href=\"http://www.google.com\">Google</a>")
+});
 
 var server = app.listen(app.get("port"), function() {
 	var port = server.address().port;
