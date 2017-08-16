11aaf14c0bc9b8cf5221d71b7abbf4c171bf6633
Levi Zitting <levizitting5663@gmail.com>
2017-08-16 14:45:22 -0500

    basic express routes

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.set('port', 3000);
+
+
+app.get('/', (req, res) => {
+	res.status(200)
+		.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/json', (req, res) => {
+	res.status(200)
+		.json({jsonData: true});
+});
+
+app.get('/html', (req, res) => {
+	res.status(200)
+		.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 const server = app.listen(app.get('port'), () => {
 	const port = server.address().port;
 	console.log(`Magic happens on port ${port}`);
 });
-
-console.log('Me first');
