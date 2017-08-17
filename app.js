8d2afacfbff80141851c12d3a6ccf06b8e71631c
Levi Zitting <levizitting5663@gmail.com>
2017-08-16 19:12:18 -0500

    basic middleware in express

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.set('port', 3000);
 
+app.use((req, res, next) => {
+	console.log(req.method, req.url);
+	next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/json', (req, res) => {
