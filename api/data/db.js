fc7be1e3fff16dbe7e1db927c322792340dd6888
Levi Zitting <levizitting5663@gmail.com>
2017-08-22 11:48:53 -0500

    mongoose connection and watching for mongoose events

---
+++ api/data/db.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const config = {
+	host: 'localhost',
+	port: '27017',
+	db: 'meanhotel'
+}
+
+const dburl = `mongodb://${config.host}:${config.port}/${config.db}`;
+
+mongoose.connect(dburl);
+
+mongoose.connection.on('connected', () => {
+	console.log(`Mongoose connected to ${dburl}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+	console.log(`Mongoose disconnected from ${dburl}`);
+});
+
+mongoose.connection.on('error', err => {
+	console.log(`Mongoose connection error: ${err}`);
+});
+
+process.on('SIGINT', () => {
+	mongoose.connection.close(() => {
+		console.log(`Mongoose disconnected from ${dburl} through app termination`);
+		process.exit(0);
+	});
+});
+
+process.on('SIGTERM', () => {
+	mongoose.connection.close(() => {
+		console.log(`Mongoose disconnected from ${dburl} through app termination`);
+		process.exit(0);
+	});
+});
+
+process.once('SIGUSR2', () => {
+	mongoose.connection.close(() => {
+		console.log(`Mongoose disconnected from ${dburl} through app termination`);
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('./api/data/dbconnection.js').open();
+require('./api/data/db.js');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
