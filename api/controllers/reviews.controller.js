d13a515e78ad0d9ff7ec03f52964cf2ad94d1035
Levi Zitting <levizitting5663@gmail.com>
2017-08-23 13:47:20 -0500

    using mongoose to select subdocuments

---
+++ api/controllers/reviews.controller.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Hotel = mongoose.model('Hotel');
+
+module.exports.index = (req, res) => {
+	const hotelId = req.params.hotelId;
+	
+		Hotel
+			.findById(hotelId)
+			.select('reviews')
+			.exec((err, hotel) => {
+				console.log(`Found hotel with id: ${hotelId}`);
+				res
+					.status(200)
+					.json(hotel.reviews);
+			});	
+}
+
+module.exports.show = (req, res) => {
+	const hotelId = req.params.hotelId;
+	const reviewId = req.params.reviewId;
+
+	Hotel
+	.findById(hotelId)
+	.select('reviews')
+	.exec((err, hotel) => {
+		const review = hotel.reviews.id(reviewId);
+		console.log(`Found review with id: ${reviewId}`);
+		res
+			.status(200)
+			.json(review);
+	});	
+}
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const ctrlHotels = require('../controllers/hotels.controller.js');
+const ctrlReviews = require('../controllers/reviews.controller.js');
 
+
+// Hotel routes
 router
 	.route('/hotels')
 	.get(ctrlHotels.index);
@@ -13,6 +16,16 @@ router
 
 router
 	.route('/hotels')
-	.post(ctrlHotels.create)
+	.post(ctrlHotels.create);
+
+// Review routes
+
+router
+	.route('/hotels/:hotelId/reviews')
+	.get(ctrlReviews.index);
+
+router
+	.route('/hotels/:hotelId/reviews/:reviewId')
+	.get(ctrlReviews.show);
 
 module.exports = router;
