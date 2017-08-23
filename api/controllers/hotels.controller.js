b97faa202b9ffce1620151f58bb0e1c16a0b8144
Levi Zitting <levizitting5663@gmail.com>
2017-08-22 20:33:51 -0500

    Using mongoose in get routes of API

--- api/controllers/hotels.controller.js
+++ api/controllers/hotels.controller.js
@@ -1,41 +1,35 @@
-const hotelData = require('../data/hotel-data.json');
-const dbconn = require('../data/dbconnection.js');
-const ObjectID = require('mongodb').ObjectId;
+const mongoose = require('mongoose');
+const Hotel = mongoose.model('Hotel');
 
 module.exports.index = (req, res) => {
-	const db = dbconn.get();
-	const collection = db.collection('hotels');
 
 	const offset = req.query && req.query.offset ? parseInt(req.query.offset) : 0;
 	const count = req.query && req.query.count ? parseInt(req.query.count) : 5;
 
-	collection
+	Hotel
 		.find()
 		.skip(offset)
 		.limit(count)
-		.toArray((err, docs) => {
+		.exec((err, hotels) => {
+			console.log(`Found ${hotels.length} hotels`);
 			res
-			.status(400)
-			.json(docs);
+				.status(200)
+				.json(hotels);
 		});
-
 }
 
 module.exports.show = (req, res) => {
-	const db = dbconn.get();
-	const collection = db.collection('hotels');
 
 	const hotelId = req.params.hotelId;
-	console.log(`GET hotelId: ${hotelId}`);
 
-	collection
-		.findOne({
-			_id: ObjectID(hotelId)
-		}, (err, doc) => {
-			res.status(200)
-			.json(doc);
+	Hotel
+		.findById(hotelId)
+		.exec((err, hotel) => {
+			console.log(`Found hotel with id: ${hotelId}`);
+			res
+				.status(200)
+				.json(hotel);
 		});
-
 }
 
 module.exports.create = (req, res) => {
