b42ea3818fe1487bedc490e4ca5d8343d1f93f75
Levi Zitting <levizitting5663@gmail.com>
2017-08-26 20:36:26 -0500

    Routing and templates

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
-angular.module('myApp', []);
+angular.module('myApp', ['ngRoute']).config(config);
+
+function config($routeProvider) {
+	$routeProvider.when('/',  {
+		templateUrl: 'templates/main.html',
+		controller: 'MyController',
+		controllerAs: 'vm'
+	})
+	.when('/about', {
+		templateUrl: 'templates/about.html',
+		controller: 'AboutController',
+		controllerAs: 'vm'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+}
--- public/js/controller.js
+++ public/js/controller.js
@@ -1,6 +1,13 @@
-angular.module('myApp').controller('MyController', MyController);
+angular.module('myApp')
+	.controller('MyController', MyController)
+	.controller('AboutController', AboutController);
 
 function MyController() {
 	const vm = this;
 	vm.name = 'Levi';
+}
+
+function AboutController() {
+	const vm = this;
+	vm.bio = 'Suh dude. I\'m a piece of garbage';
 }
